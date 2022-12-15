9155efd9f41e3b173a990c59fe108b6f5683991e
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-15 22:50:50 +0300

    fix3

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -2,12 +2,12 @@ package school.mjc.stage0.loops.task4;
 
 public class FixMe {
     public static void main(String[] args) {
-        for (int i = 0; i <= 7; i++) {
+        for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);}
-            else if (i == 7){
+            if (i == 7){
                 System.out.println("last");
-                   }}
+                    break;}}
                 System.out.println("will I be printed?");
     }
 }
